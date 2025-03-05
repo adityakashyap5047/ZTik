3bcff592b14b6cb4b0523b3176b77bdc25135bb2
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-05 23:18:44 +0530

    customizing the tailwind config file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import LeftBar from "@/components/LeftBar";
 import "./globals.css";
+import RightBar from "@/components/RightBar";
 
 export default function RootLayout({
   children,
@@ -7,7 +9,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="container flex justify-between bg-red-300 sm:bg-blue-300 bg-red">
+          <div>
+            <LeftBar/>
+          </div>
+          <div>{children}</div>
+          <div>
+            <RightBar/>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
