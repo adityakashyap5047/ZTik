ed05c783eabbb91fe273e33f738ef4d94bf30678
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-08 15:18:56 +0530

    customizing the design for logo and user

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 function LeftBar() {
   return (
-    <div>LeftBar</div>
+    <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+      {/* Logo Menu Button */}
+      <div className="">
+        {/* Logo */}
+        <Link href="/">
+          <Image src="icons/logo.svg" alt="logo" width={24} height={24}/>
+        </Link>
+      </div>
+      <div className="">
+        {/* User */}
+        user
+      </div>
+    </div>
   )
 }
 
