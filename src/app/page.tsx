7ad7e5178d24a74f7c8ae4cbda7a48adc7f2594e
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-09 14:15:23 +0530

    rendering image using image kit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+"use client"
+
+import { IKImage } from "imagekitio-next";
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
 function page() {
   return (
-    <div>page</div>
+    <div>
+      <IKImage urlEndpoint={urlEndpoint} path="/ztik/post.jpeg" width={600} height={600} alt="test post" />
+    </div>
+    
   )
 }
 
