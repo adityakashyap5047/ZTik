3a8d3bc052e482648594e57a4a37d150057521b6
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-10 14:29:51 +0530

    creating the avatar and post type component for post

---
+++ src/components/Post.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from './Image'
+
+function Post() {
+  return (
+    <div className='p-4 border-y-[1px] border-borderGray'>
+        {/* Post type */}
+        <div className="flex items-center gap-2 text-sm text-textGray mb-2 from-bold">
+            icon
+            <span>Aditya Kumar</span>
+        </div>
+        {/* Post content */}
+        <div className="flex gap-4">
+            {/* Avatar  */}
+            <div className="relative w-10 h-10 rounded-full overflow-hidden">
+                <Image path='ztik/general/avatar.png' alt='user avatar' w={100} h={100} tr={true}/>
+            </div>
+            {/* Content  */}
+            <div className="">
+                {/* Top  */}
+                <div className=""></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Post
