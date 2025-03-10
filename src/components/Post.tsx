71fc86dfbc9a4ced52b218e64feca76b281f5ecc
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-10 14:42:25 +0530

    completing the post info section

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -17,10 +17,16 @@ function Post() {
                 <Image path='ztik/general/avatar.png' alt='user avatar' w={100} h={100} tr={true}/>
             </div>
             {/* Content  */}
-            <div className="">
                 {/* Top  */}
-                <div className=""></div>
-                <PostInfo/>
+            <div className='flex-1'>
+                <div className="flex items-center justify-between gap-2">
+                    <div className="flex items-center gap-2 flex-wrap">
+                        <h1 className='text-md font-bold'>Aditya Kumar</h1>
+                        <span className='text-textGray'>@aditya</span>
+                        <span className='text-textGray'>1 day ago</span>
+                    </div>
+                    <PostInfo/>
+                </div>
             </div>
         </div>
     </div>
