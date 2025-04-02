80dd0bf5a192cd5ec926dff69ec0fe14bfb8e5ff
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-04-02 16:08:47 +0530

    complete the share component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ function page() {
       <div className="px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray">
         <Link className="pb-3 flex items-center border-b-4 border-iconBlue " href="/">For Your</Link>
         <Link className="pb-3 flex items-center" href="/">Following</Link>
-        <Link className="pb-3 flex items-center" href="/">React.js</Link>
-        <Link className="pb-3 flex items-center" href="/">Next.js</Link>
-        <Link className="pb-3 flex items-center" href="/">JavaScript</Link>
+        <Link className="pb-3 hidden md:flex items-center" href="/">React.js</Link>
+        <Link className="pb-3 hidden md:flex items-center" href="/">Next.js</Link>
+        <Link className="pb-3 hidden md:flex items-center" href="/">JavaScript</Link>
       </div>
       <Share/>
       <Feed/>
--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Image from './Image'
 
 function Share() {
   return (
-    <div>Share</div>
+    <div className="p-4 flex gap-4">
+      {/* Avatar */}
+      <div className="relative w-10 h-10 rounded-full overflow-hidden">
+        <Image path='ztik/general/avatar.png' alt='Avatar' w={100} h={100} tr={true}/>
+      </div>
+      {/* Others  */}
+      <div className="flex flex-1 flex-col gap-4">
+        <input type="text" placeholder='What is happening' className='bg-transparent outline-none placeholder:text-textGray text-xl'/>
+        <div className="flex items-center justify-between gap-4 flex-wrap">
+          <div className="flex gap-4 flex-wrap">
+            <Image path='ztik/icons/image.svg' alt='' w={20} h={20} className='cursor-pointer'/>
+            <Image path='ztik/icons/gif.svg' alt='' w={20} h={20} className='cursor-pointer'/>
+            <Image path='ztik/icons/poll.svg' alt='' w={20} h={20} className='cursor-pointer'/>
+            <Image path='ztik/icons/emoji.svg' alt='' w={20} h={20} className='cursor-pointer'/>
+            <Image path='ztik/icons/schedule.svg' alt='' w={20} h={20} className='cursor-pointer'/>
+            <Image path='ztik/icons/location.svg' alt='' w={20} h={20} className='cursor-pointer'/>
+          </div>
+          <button className='bg-white text-black font-bold rounded-full py-2 px-4'>Post</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
