17c9902ec907fe8da57527333fead3e769866797
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-04-02 16:57:45 +0530

    oppening the model when click on the edit button

---
+++ src/components/ImageEditor.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function ImageEditor({onClose, previewUrl, settings, setSettings}:{
+    onClose:()=>void,
+    previewUrl: string, 
+    settings: {
+        type: "original" | "wide" | "square",
+        sensitive: boolean
+    },
+    setSettings: React.Dispatch<React.SetStateAction<{
+        type: "original" | "wide" | "square";
+        sensitive: boolean;
+    }>>
+}) {
+  return (
+    <div className='fixed w-screen h-screen left-0 top-0 bg-black bg-opacity-75 z-10 flex items-center justify-center'>
+        <div className="bg-black rounded-xl p-12 flex flex-col gap-4">
+            Test
+        </div>
+    </div>
+  )
+}
+
+export default ImageEditor  
--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -4,10 +4,19 @@ import React, { useState } from 'react'
 import Image from './Image'
 import NextImage from "next/image";
 import { shareAction } from '@/actions';
+import ImageEditor from './ImageEditor';
 
 function Share() {
 
   const [media, setMedia] = useState<File | null>(null)
+  const [isEditorOpen, setIsEditorOpen] = useState(false)
+  const [settings, setSettings] = useState<{
+    type: "original" | "wide" | "square",
+    sensitive: boolean
+  }>({
+    type: "original",
+    sensitive: false
+  })
 
   const handleMediaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]){
@@ -29,9 +38,10 @@ function Share() {
         {
           previewURL && <div className='relative rounded-xl overflow-hidden'>
             <NextImage src={previewURL}alt='Preview Image' width={600} height={600}/>
-            <div className='absolute top-2 left-2 bg-black bg-opacity-50 text-white py-1 px-4 rounded-full font-black text-sm cursor-pointer'>Edit</div>
+            <div className='absolute top-2 left-2 bg-black bg-opacity-50 text-white py-1 px-4 rounded-full font-black text-sm cursor-pointer' onClick={()=>setIsEditorOpen(true)}>Edit</div>
           </div>
         }
+        {isEditorOpen && previewURL && <ImageEditor onClose={() => setIsEditorOpen(false)} previewUrl={previewURL} settings={settings} setSettings={setSettings}/>}
         <div className="flex items-center justify-between gap-4 flex-wrap">
           <div className="flex gap-4 flex-wrap">
             <input type="file" name='file' onChange={handleMediaChange} className='hidden' id='file'/>
